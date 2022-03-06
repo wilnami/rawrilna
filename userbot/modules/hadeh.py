9f0f90dfc98b670dc01565408db3b8042982d462
wilnami <101022475+wilnami@users.noreply.github.com>
2022-03-06 07:13:47 +0700

    menambah jeda

--- userbot/modules/hadeh.py
+++ userbot/modules/hadeh.py
@@ -1,19 +1,19 @@
 from platform import uname
+import time
 from userbot import ALIVE_NAME, CMD_HELP
 from userbot.events import register
-import asyncio
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
-loop = asyncio.get_event_loop()
 
 @register(outgoing=True, pattern='^.hadeh(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("`selamat anda telah...`")
-    # terus jeda 5 detik
-    await loop.call_later(2, typew.edit("`mendapat tambahan 1 hadeh`"))
+    # terus jeda 1 detik
+    time.sleep(1)
+    await typew.edit("`mendapat tambahan 1 hadeh`")
 
 CMD_HELP.update({
     "hadeh":
