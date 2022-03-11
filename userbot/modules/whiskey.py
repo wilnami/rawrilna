7af9645cd6201fb299e76f72188139cf4c3e22e5
wilnami <101022475+wilnami@users.noreply.github.com>
2022-03-12 00:20:31 +0700

    menambah cmd whiskey

---
+++ userbot/modules/whiskey.py
@@ -0,0 +1,110 @@
+from platform import uname
+from time import time, sleep
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern="^.whiskey (.*)")
+async def typewriter(typew):
+    counter = int(typew.pattern_match.group(1).split(' ', 2)[0])
+
+    for _ in range(1, counter):
+        await typew.edit("`PV WHISKEY\n\n-WILNA WHISKEY-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-アルディ 'だ'-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝐉𝐚𝐧𝐝𝐚 𝐝𝐞𝐬𝐚✞`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Ɓꭆᴏ͢ᴅʏ´ʷʰⁱˢᵏᵉʸ-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙺𝙴𝚉𝚉`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝑨𝒏𝒖 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Upin`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-IamRage`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-jeppa`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝐊ay♡ •`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙽𝙸𝚉𝙰𝚁 `𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝚁𝙰𝙳𝙴𝙽 𝖶𝗁𝗂𝗌𝗄𝖾𝗒-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Aca 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙽𝚈𝙴𝙷`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙼𝙰𝙺𝚂𝙸𝙼`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Ꭱyu 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ꪀꪖꪊ𝕣ꪖ 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-kenz 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴘɪᴡᴘɪᴡ`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝐁𝐨𝐭`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-⚠️Kenzo '𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ʟʏʀɪᴄᴏ•𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Silfa 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-MIKU•𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴍᴇᴋᴀʏ`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙳 𝙸 𝙽`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙸𝙲𝙰`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-⁵ᴺ¹ᴾ³ᴿ 𝚁𝙴𝚇𝚇 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-dotsky•ʂ𝚢𝚗𝚍𝚒𝚌𝚊𝚝𝚎-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴩᴀɴɪ`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴘᴀᴛʀɪᴄᴋ`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝙴𝙲𝙰`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝚜𝚊𝚗 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-⁵ᴺ¹ᴾ³ᴿ 𝐊𝐈𝐍𝐆 𝑾𝑯𝑰𝑺𝑲𝑬𝒀 ︻̷ ┻═━一-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-starla `𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-LOLY WHISKEY-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-Boy 𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴋᴇɴ `𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-𝘠𝘜𝘚𝘙𝘐𝘓`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+        sleep(1)           
+        await typew.edit("`WHISKEY ARUS BAWAH\n\n-ᴘᴀᴛʀɪᴄᴋ`𝑾𝑯𝑰𝑺𝑲𝑬𝒀-`")
+
+    await typew.delete()
+    
+# async def typewriter(typew):
+#     typew.pattern_match.group(1)
+#     while True:
+#         sleep(60 - time() % 60)
+# 	    # thing to run
+        
+        
+CMD_HELP.update({
+    "whiskey":
+    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.whiskey`\
+\n↳ : Untuk melihat daftar anggota whiskey"
+})
